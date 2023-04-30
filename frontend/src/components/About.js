a433fdb04e781406a24d0fea4986e9078d2488bb
JoseAntonio5 <jlopespaiva00@gmail.com>
2023-04-30 17:11:23 -0300

    About page new text

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -9,13 +9,20 @@ function About() {
 
             <div className='About-content'>
                 <h1>About Us</h1>
-                <p>
-                    FloraFriend is a web application that helps you keep track of your plants. You can add your plants, edit, and delete plants, as well as see all of your plants at once.
-                </p>
+                    <div className='About-content-paragraph'>
+                        <p>Welcome to FloraFriend! We are a web application that helps you keep track of your plants. You can add your plants, edit, and delete plants, as well as see all of your plants at once.</p>
+                    </div>
+                    <div className='About-content-paragraph'>
+                        <p>With our user-friendly interface, you can easily track your plants progress, including when they were last watered and when they're due for their next watering.</p>
+                    </div>
+                    <div className='About-content-paragraph'>
+                        <p>Join FloraFriend today and start enjoying all the benefits of having happy, healthy plants in your life!</p>
+                    </div>
+                <p><small>v1.0</small></p>
             </div>
         </div>
     </div>
   )
 }
 
-export default About;
+export default About
